5c8e76b880b32b206b6dee02dd61c7dac8279226
Vishakha Kumari <shikhak119@gmail.com>
2019-02-02 14:58:24 +0530

    third assignment

---
+++ assignment3.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Acad"+"view")
+num1=input("enter first number")
+print("The first value is",num1)
+num2=input("enter second number")
+print("The second value is",num2)
+num3=input("enter third number")
+print("The third value is",num3)
+s="Acadview"
+course="Python"
+fees=5000
+print('%s %s %d'%(s,course,fees))
+name="Tony stark"
+salary=1000000
+print('%s %d'%(name,salary))
+    
